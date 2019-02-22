a8b3dd378bb0970ca0d25402c1a5f863befc1937
unknown <imed_jebari@hotmail.fr>
2019-02-22 23:42:55 +0100

    add my firebase information

---
+++ src/config/imed-data-config.js
@@ -0,0 +1,7 @@
+
+exports.API_KEY = "AIzaSyCfe9unhY825Sn6VNrd0byP66PnUXlUFZM" ;
+exports.AUTH_DOMAIN = "imed-fire-react.firebaseapp.com" ;
+exports.DATABASE_URL = "https://imed-fire-react.firebaseio.com" ;
+exports.PROJECT_ID = "imed-fire-react" ;
+exports.STORAGE_BUCKET = "imed-fire-react.appspot.com" ;
+exports.MESSAGING_SENDER_ID = "163771751992" ;
