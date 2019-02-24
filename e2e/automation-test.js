c517c15c4930e573e18899a40adf00d564f12a96
unknown <imed_jebari@hotmail.fr>
2019-02-24 13:48:06 +0100

    my first simple automation test

--- e2e/automation-test.js
+++ e2e/automation-test.js
@@ -1,34 +1,34 @@
 const webdriver = require('selenium-webdriver');
 const By = webdriver.By ;
 
+const mySimpleAutomateTest = async () => {
+        try{
 
-     
-const basicExample = async () => {
-          try{
-              var driver = new webdriver.Builder().forBrowser('chrome').build();
+            var driver = new webdriver.Builder().forBrowser('chrome').build();
       
-              await driver.get('http://localhost:3000/');
+            await driver.get('http://localhost:3000/');
       
-              await driver.getTitle().then(function(title) {
+            await driver.getTitle().then(function(title) {
                         //  console.log("The title is: " + title)
-                  });
+                });
 
-             await driver.findElement(By.css('.btnSort')).getText().then(function(text){
+            await driver.findElement(By.css('.btnSort')).getText().then(function(text){
                            //   console.log("The text is: " + text)
-             });
+                });
 
-             await setTimeout(function(){
+            await setTimeout(function(){
                     driver.findElement(By.css('.btnSort')).click();
-             },2000)
-             await setTimeout(function(){
-                    driver.quit();
-             },3000)
+                },2000);
 
-          }
-      
-          catch(err){
-              handleFailure(err, driver)
-          }
-      }
-      
-      basicExample();
+            await setTimeout(function(){
+                    driver.quit();
+                },3000);
+
+        }catch(err){
+
+              handleFailure(err, driver);
+
+        }
+};
+   
+    mySimpleAutomateTest();
